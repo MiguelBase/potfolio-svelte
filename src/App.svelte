45fa18d3279f8762ff9958d98f9c225ba762a37b
MiguelBase <miguel.huayllas@basetis.com>
2022-10-28 16:39:13 +0200

    update package.json

--- src/App.svelte
+++ src/App.svelte
@@ -7,29 +7,13 @@
 </script>
 
 <main>
-  <div>
+  
 
-  <div class="container">
+  <div>
     <Navbar />
     <Counter />
     
   </div>
-  <div class="columns">
-    <div class="column">
-      First column
-    </div>
-    <div class="column">
-      Second column
-    </div>
-    <div class="column">
-      Third column
-    </div>
-    <div class="column">
-      Fourth column
-    </div>
-  </div>
-
-
 
 </main>
 
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,43 +1,54 @@
 <script>
   import { onDestroy } from "svelte/internal";
   import dataStore from "../store";
-  import {es_nav_titles} from "../webContent";
-  import {en_nav_titles} from "../webContent";
-  import {current_nav_titles} from "../webContent";
+  import { es_nav_titles } from "../webContent";
+  import { en_nav_titles } from "../webContent";
+  import { current_nav_titles } from "../webContent";
   let time = null;
   const unsubStore = dataStore.subscribe((data) => (time = data));
   onDestroy(unsubStore);
-  
-  const change_nav_titles_to_spanish = () =>{ current_nav_titles.update(()=> {return $es_nav_titles})}
-  const change_nav_titles_to_english = () =>{  current_nav_titles.update(() => {return $en_nav_titles})}
-  
-  change_nav_titles_to_spanish()
 
+  const change_nav_titles_to_spanish = () => {
+    current_nav_titles.update(() => {
+      return $es_nav_titles;
+    });
+  };
+  const change_nav_titles_to_english = () => {
+    current_nav_titles.update(() => {
+      return $en_nav_titles;
+    });
+  };
 
+  change_nav_titles_to_spanish();
 </script>
 
-<nav class="navbar">
-  <div>
-    {$current_nav_titles.profile}
-  </div>
-  <div>
-    {$current_nav_titles.projects}
-  </div>
-  <div>
-    {$current_nav_titles.tecnologies}
-  </div>
-  <div>
-    {$current_nav_titles.social}
-    <div>
-        {$current_nav_titles.contact}
+<nav class="container is-desktop">
+  <div class="columns">
+    <div class="column">
+      {$current_nav_titles.profile}
+    </div>
+    <div class="column">
+      {$current_nav_titles.projects}
+    </div>
+    <div class="column">
+      {$current_nav_titles.tecnologies}
+    </div>
+    <div class="column">
+      {$current_nav_titles.social}
+    </div>
+    <div class="column">
+      {$current_nav_titles.contact}
     </div>
   </div>
+  <div class="columns">
+    <button class="column">Change to English</button>
+    <button class="column">Cambair a español</button>
 
-  <button class="button is-primary">Primary</button>
+  </div>
 </nav>
+
 <style>
-    nav {
-        background-color: rgb(148, 148, 81);
-        width:400px;
-    }
+  nav {
+    background-color: rgb(148, 148, 81);
+  }
 </style>
